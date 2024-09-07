e005fc910669bb54321de935c7a13929820137ed
PeepoFrog <organizm999@gmail.com>
2024-09-07 15:56:14 +0300

    feat: app toml editor tab

---
+++ gui/screen_config_editor.go
@@ -0,0 +1,83 @@
+package gui
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/widget"
+	"github.com/KiraCore/kensho/helper/httph"
+)
+
+func makeCfgEditorScreen(_ fyne.Window, g *Gui) fyne.CanvasObject {
+	appTomlTab := container.NewTabItem("app.toml", makeAppTomlTab(g))
+	configTomlTab := container.NewTabItem("config.toml", makeConfigTomlTab(g))
+	tabsMenu := container.NewAppTabs(appTomlTab, configTomlTab)
+
+	return tabsMenu
+}
+
+func makeAppTomlTab(g *Gui) fyne.CanvasObject {
+	configBinding := binding.NewString()
+	configEditor := widget.NewEntryWithData(configBinding)
+	configEditor.MultiLine = true
+	configEditor.Disable()
+
+	const editButtonEnabledState = "Disable editing"
+	const editButtonDisabledState = "Enable editing"
+
+	editButton := widget.NewButton(editButtonDisabledState, func() {})
+	saveButton := widget.NewButton("Save", func() {})
+	saveButton.Disable()
+	refreshButton := widget.NewButton("Refresh", func() {})
+
+	refreshFunc := func() {
+		g.WaitDialog.ShowWaitDialog()
+		cfg, err := httph.GetAppTomlConfig(g.sshClient, 8282)
+		if err != nil {
+			g.WaitDialog.HideWaitDialog()
+			g.showErrorDialog(err, binding.NewDataListener(func() {}))
+		}
+		configBinding.Set(cfg)
+
+		g.WaitDialog.HideWaitDialog()
+	}
+
+	saveFunc := func() {
+		g.WaitDialog.ShowWaitDialog()
+		cfg, _ := configBinding.Get()
+		err := httph.SetAppTomlConfig(g.sshClient, cfg, 8282)
+		if err != nil {
+			g.WaitDialog.HideWaitDialog()
+			g.showErrorDialog(err, binding.NewDataListener(func() {}))
+		}
+
+		g.WaitDialog.HideWaitDialog()
+	}
+
+	editFunc := func() {
+		if configEditor.Disabled() {
+			editButton.SetText(editButtonDisabledState)
+			saveButton.Enable()
+			configEditor.Enable()
+		} else {
+			saveButton.Disable()
+			editButton.SetText(editButtonEnabledState)
+			configEditor.Disable()
+		}
+	}
+
+	editButton.OnTapped = editFunc
+	refreshButton.OnTapped = refreshFunc
+	saveButton.OnTapped = saveFunc
+
+	buttonsContainer := container.NewVBox(
+		container.NewHBox(editButton, saveButton),
+		refreshButton,
+	)
+
+	return container.NewBorder(nil, buttonsContainer, nil, nil, configEditor)
+}
+
+func makeConfigTomlTab(g *Gui) fyne.CanvasObject {
+	return container.NewBorder(nil, nil, nil, nil)
+}
