c4c02d32a2f5ff7dc434a8b5682a2abaeefe4334
PeepoFrog <organizm999@gmail.com>
2024-09-11 14:47:26 +0300

    feat: config editor tab code refactor

--- gui/screen_config_editor.go
+++ gui/screen_config_editor.go
@@ -9,14 +9,46 @@ import (
 )
 
 func makeCfgEditorScreen(_ fyne.Window, g *Gui) fyne.CanvasObject {
-	appTomlTab := container.NewTabItem("app.toml", makeAppTomlTab(g))
-	configTomlTab := container.NewTabItem("config.toml", makeConfigTomlTab(g))
+	appTomlTab := container.NewTabItem("app.toml", makeTextEditTab(
+		g,
+		func(cfg string) error {
+			err := httph.SetAppTomlConfig(g.sshClient, cfg, 8282)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+		func() (string, error) {
+			cfg, err := httph.GetAppTomlConfig(g.sshClient, 8282)
+			if err != nil {
+				return "", err
+			}
+			return cfg, nil
+		},
+	))
+	configTomlTab := container.NewTabItem("config.toml", makeTextEditTab(
+		g,
+		func(cfg string) error {
+			err := httph.SetConfigTomlConfig(g.sshClient, cfg, 8282)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+		func() (string, error) {
+			cfg, err := httph.GetConfigTomlConfig(g.sshClient, 8282)
+			if err != nil {
+				return "", err
+			}
+			return cfg, nil
+		},
+	))
 	tabsMenu := container.NewAppTabs(appTomlTab, configTomlTab)
 
 	return tabsMenu
 }
 
-func makeAppTomlTab(g *Gui) fyne.CanvasObject {
+func makeTextEditTab(g *Gui, saveFile func(string) error, getFile func() (string, error)) fyne.CanvasObject {
 	configBinding := binding.NewString()
 	configEditor := widget.NewEntryWithData(configBinding)
 	configEditor.MultiLine = true
@@ -32,7 +64,7 @@ func makeAppTomlTab(g *Gui) fyne.CanvasObject {
 
 	refreshFunc := func() {
 		g.WaitDialog.ShowWaitDialog()
-		cfg, err := httph.GetAppTomlConfig(g.sshClient, 8282)
+		cfg, err := getFile()
 		if err != nil {
 			g.WaitDialog.HideWaitDialog()
 			g.showErrorDialog(err, binding.NewDataListener(func() {}))
@@ -45,70 +77,7 @@ func makeAppTomlTab(g *Gui) fyne.CanvasObject {
 	saveFunc := func() {
 		g.WaitDialog.ShowWaitDialog()
 		cfg, _ := configBinding.Get()
-		err := httph.SetAppTomlConfig(g.sshClient, cfg, 8282)
-		if err != nil {
-			g.WaitDialog.HideWaitDialog()
-			g.showErrorDialog(err, binding.NewDataListener(func() {}))
-		}
-
-		g.WaitDialog.HideWaitDialog()
-		refreshFunc()
-	}
-
-	editFunc := func() {
-		if configEditor.Disabled() {
-			editButton.SetText(editButtonEnabledState)
-			saveButton.Enable()
-			configEditor.Enable()
-		} else {
-			saveButton.Disable()
-			editButton.SetText(editButtonDisabledState)
-			configEditor.Disable()
-		}
-	}
-
-	editButton.OnTapped = editFunc
-	refreshButton.OnTapped = refreshFunc
-	saveButton.OnTapped = saveFunc
-
-	buttonsContainer := container.NewVBox(
-		container.NewGridWithColumns(2, editButton, saveButton),
-		refreshButton,
-	)
-	refreshFunc()
-	return container.NewBorder(nil, buttonsContainer, nil, nil, configEditor)
-}
-
-func makeConfigTomlTab(g *Gui) fyne.CanvasObject {
-	configBinding := binding.NewString()
-	configEditor := widget.NewEntryWithData(configBinding)
-	configEditor.MultiLine = true
-	configEditor.Disable()
-
-	const editButtonEnabledState = "Disable editing"
-	const editButtonDisabledState = "Enable editing"
-
-	editButton := widget.NewButton(editButtonDisabledState, func() {})
-	saveButton := widget.NewButton("Save", func() {})
-	saveButton.Disable()
-	refreshButton := widget.NewButton("Refresh", func() {})
-
-	refreshFunc := func() {
-		g.WaitDialog.ShowWaitDialog()
-		cfg, err := httph.GetConfigTomlConfig(g.sshClient, 8282)
-		if err != nil {
-			g.WaitDialog.HideWaitDialog()
-			g.showErrorDialog(err, binding.NewDataListener(func() {}))
-		}
-		configBinding.Set(cfg)
-
-		g.WaitDialog.HideWaitDialog()
-	}
-
-	saveFunc := func() {
-		g.WaitDialog.ShowWaitDialog()
-		cfg, _ := configBinding.Get()
-		err := httph.SetConfigTomlConfig(g.sshClient, cfg, 8282)
+		err := saveFile(cfg)
 		if err != nil {
 			g.WaitDialog.HideWaitDialog()
 			g.showErrorDialog(err, binding.NewDataListener(func() {}))
